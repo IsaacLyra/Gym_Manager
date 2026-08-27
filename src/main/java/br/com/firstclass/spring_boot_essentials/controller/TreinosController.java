693fba2f8d7b5a73007c4dc96c4b1e412e70a9f8
Isaac Lyra <isaacprolog@gmail.com>
2026-08-26 21:16:48 -0300

    Update validations

---
+++ src/main/java/br/com/firstclass/spring_boot_essentials/controller/TreinosController.java
@@ -0,0 +1,40 @@
+package br.com.firstclass.spring_boot_essentials.controller;
+
+import br.com.firstclass.spring_boot_essentials.database.model.AvaliacoesFisicasEntity;
+import br.com.firstclass.spring_boot_essentials.database.model.ExerciciosEntity;
+import br.com.firstclass.spring_boot_essentials.exception.BadRequestException;
+import br.com.firstclass.spring_boot_essentials.exception.NotFoundException;
+import br.com.firstclass.spring_boot_essentials.service.AlunosService;
+import br.com.firstclass.spring_boot_essentials.service.ExerciciosService;
+import dto.AlunosDto;
+import dto.ExercicioDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/alunos")
+@RequiredArgsConstructor
+@Validated
+public class AlunosController {
+
+    private final AlunosService alunosService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+
+    public void criarAluno(@Valid @RequestBody AlunosDto alunosDto) throws BadRequestException {
+        alunosService.criarAluno(alunosDto);
+    }
+
+    @GetMapping("/{alunoId}/avaliacao")
+
+    public AvaliacoesFisicasEntity getAvaliacoesFiscas(@PathVariable Integer alunoId) throws NotFoundException {
+        return alunosService.getAlunoAvaliacao(alunoId);
+    }
+
+}
