2a454796e1400c8d5f7b131c0b17388d271da8bd
Isaac Lyra <isaacprolog@gmail.com>
2026-09-01 16:49:46 -0300

    I forget @Rest controller

--- src/main/java/br/com/firstclass/spring_boot_essentials/controller/AuthController.java
+++ src/main/java/br/com/firstclass/spring_boot_essentials/controller/AuthController.java
@@ -7,13 +7,12 @@ import dto.RegisterRequestDto;
 import dto.TokenResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/auth")
 public class AuthController {
