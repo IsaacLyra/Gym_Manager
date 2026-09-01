26eeabd97abe4d59b9a5ac9794973023384fcbe6
Isaac Lyra <isaacprolog@gmail.com>
2026-09-01 15:51:16 -0300

    jwtFilter update

--- src/main/java/br/com/firstclass/spring_boot_essentials/config/JwtAuthenticationFilter.java
+++ src/main/java/br/com/firstclass/spring_boot_essentials/config/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -23,33 +24,38 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final TokenProvider tokenProvider;
     private final UserDetailsService userDetailsService; // interface que extrai username
 
+
     @Override
-    protected void doFilterInternal(@Nullable HttpServletRequest request,
-                                    @Nullable   HttpServletResponse response,
-                                    @Nullable   FilterChain filterChain) throws ServletException, IOException {
-
-        String autozirationHeader = request.getHeader("Authorization");
-        if(StringUtils.hasText(autozirationHeader) && autozirationHeader.startsWith("Bearer ")){
-            // validação
-            String token = autozirationHeader.substring(7);
-
-            if(tokenProvider.isTokenValid(token)){
-                String username =tokenProvider.getUsername(token);
-                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-
-
-            }
-
-        }
-
-         filterChain.doFilter(request, response);
-
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException{
+        return request.getServletPath().startsWith("/v1/auth/");
     }
 
     @Override
-        protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException{
-        return request.getRequestURI().startsWith("/v1/auth/");
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull    HttpServletResponse response,
+                                    @NonNull    FilterChain filterChain) throws ServletException, IOException {
+
+        String autozirationHeader = request.getHeader("Authorization");
+
+    if(autozirationHeader == null || !autozirationHeader.startsWith("Bearer ")){
+        filterChain.doFilter(request, response);
+        return;
+        }
+
+        String token = autozirationHeader.substring(7);
+
+        if(tokenProvider.isTokenValid(token)){
+            String username = tokenProvider.getUsername(token);
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+
+
+         filterChain.doFilter(request, response);
+
     }
 }
