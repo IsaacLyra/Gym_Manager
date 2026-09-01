0cbcb4cf11c325e1b85d74f1978c9c61834c1467
Isaac Lyra <isaacprolog@gmail.com>
2026-09-01 16:46:38 -0300

    AuthController update

--- src/main/java/br/com/firstclass/spring_boot_essentials/controller/AuthController.java
+++ src/main/java/br/com/firstclass/spring_boot_essentials/controller/AuthController.java
@@ -4,9 +4,9 @@ import br.com.firstclass.spring_boot_essentials.exception.BadRequestException;
 import br.com.firstclass.spring_boot_essentials.service.AuthenticationService;
 import dto.LoginRequestDto;
 import dto.RegisterRequestDto;
+import dto.TokenResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,17 +21,14 @@ public class AuthController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
-    // ALTERAÇÃO 3: Retornando ResponseEntity para definir o HTTP Status 201 Created
-    public ResponseEntity<Void> register(@RequestBody @Valid RegisterRequestDto registerRequestDto) throws BadRequestException {
+    public void  register(@RequestBody @Valid RegisterRequestDto registerRequestDto) throws BadRequestException {
         authenticationService.Register(registerRequestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+
     }
 
     @PostMapping("/login")
-    // ALTERAÇÃO 4: Retornando ResponseEntity contendo a resposta (ex: Token) ou OK 200
-    public ResponseEntity<?> login(@RequestBody @Valid LoginRequestDto loginRequestDto) throws Exception {
-        var token = authenticationService.login(loginRequestDto);
-        return ResponseEntity.ok(token); // Ou ResponseEntity.ok().build() se o método de service for void
+    public TokenResponseDto login(@RequestBody @Valid LoginRequestDto loginRequestDto) throws Exception {
+        return authenticationService.login(loginRequestDto);
     }
 }
 
